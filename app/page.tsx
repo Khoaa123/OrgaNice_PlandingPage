7b73fb743436b51837cdbbfe273aafa4c118a878
Tan Khoa <nguyentankhoa23082002@hotmail.com>
2026-03-09 14:23:59 +0700

    update code

--- app/page.tsx
+++ app/page.tsx
@@ -64,9 +64,9 @@ export default function Home() {
                   <span className="relative inline-flex rounded-full h-2 w-2 bg-indigo-600"></span>
                 </span>
                 <p>
-                  <span className="text-indigo-600">v1.1.12</span>
+                  <span className="text-indigo-600">v1.1.13</span>
                   <span className="mx-2 text-slate-300">|</span>
-                  Tab Rules & Side Panel upgrade
+                  Custom Icons & Tab Rules upgrade
                 </p>
               </div>
             </div>
