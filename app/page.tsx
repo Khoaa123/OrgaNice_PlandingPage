f5316b62fc912d19ffcfbf1a863fd47c602e1f33
Tan Khoa <nguyentankhoa23082002@hotmail.com>
2026-03-15 16:25:04 +0700

    update code

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
                   <span className="relative inline-flex rounded-full h-2 w-2 bg-indigo-600"></span>
                 </span>
                 <p>
-                  <span className="text-indigo-600">v1.1.16</span>
+                  <span className="text-indigo-600">v1.1.17</span>
                   <span className="mx-2 text-slate-300">|</span>
                   Popup Menu, Ignored URLs, Data Reset & Rule Export
                 </p>
