a28f5c9fb3d6a36083bd7ee62d58885a3d176b07
Tan Khoa <nguyentankhoa23082002@hotmail.com>
2026-03-23 11:01:55 +0700

    update code

--- app/page.tsx
+++ app/page.tsx
@@ -64,9 +64,9 @@ export default function Home() {
                   <span className="relative inline-flex rounded-full h-2 w-2 bg-indigo-600"></span>
                 </span>
                 <p>
-                  <span className="text-indigo-600">v1.1.20</span>
+                  <span className="text-indigo-600">v1.1.21</span>
                   <span className="mx-2 text-slate-300">|</span>
-                  Default Collection, Custom New Tab URL
+                  Cleaner popup menu, clearer system texts
                 </p>
               </div>
             </div>
