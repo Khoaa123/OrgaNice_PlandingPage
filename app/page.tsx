717669bb7e8d8e13dd022d153e18a3bb97cbb7ee
Tan Khoa <nguyentankhoa23082002@hotmail.com>
2026-03-28 21:38:47 +0700

    update code

--- app/page.tsx
+++ app/page.tsx
@@ -64,10 +64,9 @@ export default function Home() {
                   <span className="relative inline-flex rounded-full h-2 w-2 bg-indigo-600"></span>
                 </span>
                 <p>
-                  <span className="text-indigo-600">v1.1.22</span>
+                  <span className="text-indigo-600">v1.1.23</span>
                   <span className="mx-2 text-slate-300">|</span>
-                  Tab context menu, background tab opening, quick collection
-                  creation
+                  Merge multiple collections, better quick notes previews
                 </p>
               </div>
             </div>
